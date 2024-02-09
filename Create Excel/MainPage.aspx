309d5e1a199623c16cbea2c3fe91e52b20088a48
AnbazhaganCode <151020067+AnbazhaganCode@users.noreply.github.com>
2024-02-09 09:34:44 +0530

    Add files via upload

---
+++ Create Excel/MainPage.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainPage.aspx.cs" Inherits="Create_Excel.MainPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">  
+        <div>  
+            <asp:Label ID="labelId" runat="server">User Name</asp:Label>  
+            <asp:TextBox ID="UserName" runat="server" ToolTip="Enter User Name"></asp:TextBox>  
+             <br />   
+             <br />   
+            <asp:Label ID="label1" runat="server">Password</asp:Label>
+            <asp:TextBox ID="Password" runat="server" ToolTip="Enter password"></asp:TextBox>
+        </div>  
+        <p> 
+            <asp:Button ID="SubmitButton" runat="server" Text="Submit" OnClick="SubmitButton_Click" />  
+        </p>  
+        <br />                   
+    </form>  
+     <asp:Label ID="userInput" runat="server"></asp:Label>  
+</body>
+</html>
---
+++ Create Excel/MainPage.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Syncfusion.XlsIO;
+using System.Drawing;
+
+namespace Create_Excel
+{
+    public partial class MainPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            if (UserName.Text.Length == 0)
+            {
+                userInput.Text = "Please fill the User name properly";
+            }
+            else if (Password.Text.Length == 0)
+            {
+                userInput.Text = "Please fill the password properly";
+            }
+            else if (!Password.Text.Contains("@"))
+            {
+                userInput.Text = "Please enter the strong possword with @ symbol";
+            }
+            else if (Password.Text.Length < 8)
+            {
+                userInput.Text = "Please enter the strong possword with atleast 8 characters";
+            }
+            else
+            {
+                userInput.Text = "Wellcome.. \n Usser name: " + UserName.Text + " Password: " + Password.Text;
+            }
+        }
+    }
+}
